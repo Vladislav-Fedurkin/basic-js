de0fbbff337c724eb5a1049ddf1f4fe363d45ff9
Vladislav-Fedurkin <vladislav.fedurkin@gmail.com>
2021-03-28 19:59:55 +0300

    feat: the task dream-team is ready

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,8 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(cats) {
+  cats.flat();
+  for(let i = 0; i < cats.length; i++) {
+
+  }
 };
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,12 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if(!members || !members.length) return false;
+  let firstLetters = [];
+  members.map(str => {
+    if(typeof(str) === 'string' && str !== ''){
+      firstLetters.push(str.trimLeft()[0].toUpperCase());
+    }
+  })
+  return firstLetters.sort().join("")
 };
